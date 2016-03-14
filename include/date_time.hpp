0e5d4d1ee0ad409902a93e9ec80460fe96d6466b
GU Junhua <jhgu@nao.cas.cn>
2016-03-14 11:08:41 +0800

    added date_time

---
+++ include/date_time.hpp
@@ -0,0 +1,8 @@
+#ifndef DATE_TIME_HPP
+#define DATE_TIME_HPP
+#include <string>
+
+int month2num(const std::string& month_name);
+double parse_21cma_date(const std::string& date_string);
+
+#endif
---
+++ src/date_time.cpp
@@ -0,0 +1,37 @@
+#include <date_time.hpp>
+#include <cassert>
+#include <casa/Quanta/MVEpoch.h>
+#include <casa/Quanta/MVTime.h>
+#include <sstream>
+
+using namespace std;
+int month2num(const std::string& month_name)
+{
+  const static std::string month_names[]={"Jan","Feb","Mar",
+					  "Apr","May","Jun",
+					  "Jul","Aug","Sep",
+					  "Oct","Nov","Dec"};
+  for(int i=0;i<12;++i)
+    {
+      if(month_name==month_names[i])
+	{
+	  return i+1;
+	}
+    }
+  assert(0);
+  return 0;
+}
+
+double parse_21cma_date(const std::string& date_string)
+{
+  using namespace casa;
+  std::string wd,mn,day,time,year;
+  istringstream iss(date_string);
+  iss >> wd >> mn >> day >> time >> year;
+  std::string date_string2=year+"-"+to_string(month2num(mn))+"-"+day
+    +"T"+time+"+8";
+  Quantity qn;
+  std::cerr<<date_string2<<std::endl;
+  MVTime::read(qn,date_string2);
+  return qn.getValue("s");
+}
