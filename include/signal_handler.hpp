81f31f7c15723bcba74f181fb913e87ee228724e
GU Junhua <jhgu@nao.cas.cn>
2021-05-22 00:02:03 +0800

    new file:   include/signal_handler.hpp

---
+++ include/signal_handler.hpp
@@ -0,0 +1,31 @@
+#ifndef SIGNAL_HANDLER
+#define SIGNAL_HANDLER
+
+#include <signal.h>
+#include <cstdlib>
+#include <cstdio>
+#include <unistd.h>
+
+static bool running=true;
+
+static void ctrl_c_handler(int s){
+    printf("Caught signal %d\n",s);
+    running=false;
+}
+
+
+static class signal_reg{
+
+public:
+    signal_reg(){
+        struct sigaction sigIntHandler;
+        sigIntHandler.sa_handler = ctrl_c_handler;
+        sigemptyset(&sigIntHandler.sa_mask);
+        sigIntHandler.sa_flags = 0;
+
+        sigaction(SIGINT, &sigIntHandler, NULL);
+    }
+}_signal_reg;
+
+
+#endif
