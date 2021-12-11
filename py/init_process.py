d6a52ade34bb5162b06f55c4c12c0375ea16cc0f
Gu Junhua <jhgu@nao.cas.cn>
2021-12-11 09:58:56 +0800

    modified:   py/init_process.py

--- py/init_process.py
+++ py/init_process.py
@@ -1,21 +1,24 @@
 #vis_prefix="6144:6400"
 complist="components.cl"
 model=""
+refant='E01'
 
 vis=vis_prefix+".MS"
 flagversion="init.flag"
+img_prefix=vis_prefix+"_img"
+caltable=vis_prefix+".cal"
 
 flagdata(vis=vis,mode="tfcrop",autocorr=False,inpfile="",reason="any",tbuff=0.0,spw="",field="",antenna="",uvrange="",timerange="",correlation="",scan="",intent="",array="",observation="",feed="",clipminmax=[],datacolumn="DATA",clipoutside=True,channelavg=False,timeavg=False,timebin="0s",clipzeros=False,quackinterval=1.0,quackmode="beg",quackincrement=False,tolerance=0.0,addantenna="",lowerlimit=0.0,upperlimit=90.0,ntime="scan",combinescans=False,timecutoff=4.0,freqcutoff=3.0,timefit="line",freqfit="poly",maxnpieces=7,flagdimension="freqtime",usewindowstats="none",halfwin=1,extendflags=True,winsize=3,timedev="",freqdev="",timedevscale=5.0,freqdevscale=5.0,spectralmax=1000000.0,spectralmin=0.0,extendpols=True,growtime=50.0,growfreq=50.0,growaround=False,flagneartime=False,flagnearfreq=False,minrel=0.0,maxrel=1.0,minabs=0,maxabs=-1,spwchan=False,spwcorr=False,basecnt=False,fieldcnt=None,name="Summary",action="apply",display="",flagbackup=True,savepars=False,cmdreason="",outfile="",writeflags=None)
 
 flagmanager(vis=vis,mode="save",versionname="init.flag",oldname="",comment="",merge="replace")
 
-img_prefix=vis_prefix+"_img"
+
 ft(vis=vis,field="",spw="",model=model,nterms=1,reffreq="",complist=complist,incremental=False,usescratch=True)
 
 
-caltable=vis_prefix+".cal"
 
-bandpass(vis=vis,caltable=caltable,field="",spw="",intent="",selectdata=False,timerange="",uvrange="",antenna="",scan="",observation="",msselect="",solint="5min",combine="scan",refant="E01",minblperant=4,minsnr=2,solnorm=False,bandtype="B",smodel=[],append=False,fillgaps=0,degamp=3,degphase=3,visnorm=False,maskcenter=0,maskedge=5,docallib=False,callib="",gaintable=[],gainfield=[],interp=[],spwmap=[],parang=False)
+
+bandpass(vis=vis,caltable=caltable,field="",spw="",intent="",selectdata=False,timerange="",uvrange="",antenna="",scan="",observation="",msselect="",solint="5min",combine="scan",refant=refant,minblperant=4,minsnr=2,solnorm=False,bandtype="B",smodel=[],append=False,fillgaps=0,degamp=3,degphase=3,visnorm=False,maskcenter=0,maskedge=5,docallib=False,callib="",gaintable=[],gainfield=[],interp=[],spwmap=[],parang=False)
 
 applycal(vis=vis,field="",spw="",intent="",selectdata=False,timerange="",uvrange="",antenna="",scan="",observation="",msselect="",docallib=False,callib="",gaintable=[caltable],gainfield=[],interp=[],spwmap=[],calwt=[True],parang=False,applymode="",flagbackup=True)
 
